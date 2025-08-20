354c2cbefdfcd2a2e5bca20bc8a22896caab25fd
monn <pphan@paragoniu.edu.kh>
2025-08-20 15:09:31 +0700

    type 1 raycaster game

--- peepee.c
+++ peepee.c
@@ -5,9 +5,9 @@
 #include <stdlib.h>
 #include <math.h>
 #define PI 3.1415926535
-#define P2 PI/2
-#define P3 3*PI/2
-#define DR 0.0174533 //one degree in radians
+#define P2 PI / 2
+#define P3 3 * PI / 2
+#define DR 0.0174533 // one degree in radians
 
 float px, py, pdx, pdy, pa; // player angle
 int mapX = 8, mapY = 8, mapS = 64;
@@ -24,55 +24,175 @@ int map[8][8] =
         {1, 1, 1, 1, 1, 1, 1, 1},
 };
 
-float dist (float ax, float ay, float bx, float by, float ang)
-{ 
-    return ( sqrt ( (bx-ax) * (bx-ax) + (by-ay) * (by-ay)) );
+float dist(float ax, float ay, float bx, float by, float ang)
+{
+    return (sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay)));
 };
 
 void castRay2D()
 {
     int r, mx, my, mp, dof;
-    float rx, ry, ra, xo, yo;
-    
-    ra=pa-DR*30; if(ra<0){ra+=2*PI;} if (ra>2*PI) {ra-=2*PI;}
+    float rx, ry, ra, xo, yo, disT;
+
+    glLineWidth(8);
     for (r = 0; r < 64; r++)
     {
-        // // -- Check horizontal -- //
+        // Draw ceiling
+        glColor3f(0.5, 0.5, 1.0); 
+        glBegin(GL_LINES);
+        glVertex2i(r * 8 + 530, 0);
+        glVertex2i(r * 8 + 530, 160); 
+        glEnd();
+
+        // Draw floor
+        glColor3f(0.3, 0.8, 0.3);
+        glBegin(GL_LINES);
+        glVertex2i(r * 8 + 530, 160);
+        glVertex2i(r * 8 + 530, 320); 
+        glEnd();
+    }
+
+    ra = pa - DR * 30;
+    if (ra < 0)
+        ra += 2 * PI;
+    if (ra > 2 * PI)
+        ra -= 2 * PI;
+
+    for (r = 0; r < 64; r++)
+    {
+        // Check horizontal lines
         dof = 0;
-        float disH=1000000, hx=px, hy=py;
+        float disH = 1000000, hx = px, hy = py;
         float aTan = -1 / tan(ra);
-        if (ra > PI){ry = (((int)py >> 6) << 6) - 1e-4f; rx = (py - ry) * aTan + px; yo = -64; xo = -yo * aTan;} //up
-        if (ra < PI){ry = (((int)py >> 6) << 6) + 64; rx = (py - ry) * aTan + px; yo = 64; xo = -yo * aTan;} //down
-        if (fabs(ra) < 1e-6f || fabs(ra - PI) < 1e-6f) {rx = px; ry = py; dof = 8;} //left or right
+        if (ra > PI)
+        {
+            ry = (((int)py >> 6) << 6) - 1e-4f;
+            rx = (py - ry) * aTan + px;
+            yo = -64;
+            xo = -yo * aTan;
+        }
+        if (ra < PI)
+        {
+            ry = (((int)py >> 6) << 6) + 64;
+            rx = (py - ry) * aTan + px;
+            yo = 64;
+            xo = -yo * aTan;
+        }
+        if (fabs(ra) < 1e-6f || fabs(ra - PI) < 1e-6f)
+        {
+            rx = px;
+            ry = py;
+            dof = 8;
+        }
         while (dof < 8)
         {
-            mx = (int)(rx) >> 6; my = (int)(ry) >> 6; mp = my * mapX + mx;
-            if (mx >= 0 && mx < mapX && my >= 0 && my < mapY && mp > 0 && map[my][mx] == 1){ hx = rx; hy = ry; disH = dist(px, py, hx, hy, ra); dof = 8;}
-            else {rx += xo; ry += yo; dof += 1;}
+            mx = (int)(rx) >> 6;
+            my = (int)(ry) >> 6;
+            if (mx >= 0 && mx < mapX && my >= 0 && my < mapY && map[my][mx] == 1)
+            {
+                hx = rx;
+                hy = ry;
+                disH = dist(px, py, hx, hy, ra);
+                dof = 8;
+            }
+            else
+            {
+                rx += xo;
+                ry += yo;
+                dof += 1;
+            }
         }
 
-        // -- Check vertical -- //
+        // Check vertical lines
         dof = 0;
-        float disV=1000000, vx=px, vy=py;
+        float disV = 1000000, vx = px, vy = py;
         float nTan = -tan(ra);
-        if (ra>P2 && ra<P3){rx = (((int)px >> 6) << 6) - 1e-4f; ry = (px - rx) * nTan + py; xo = -64; yo = -xo * nTan;} //left
-        if (ra<P2 || ra>P3){rx = (((int)px >> 6) << 6) + 64; ry = (px - rx) * nTan + py; xo = 64; yo = -xo * nTan;} //right
-        if (fabs(ra) < 1e-6f || fabs(ra - PI) < 1e-6f) {rx = px; ry = py; dof = 8;} //up or down
+        if (ra > P2 && ra < P3)
+        {
+            rx = (((int)px >> 6) << 6) - 1e-4f;
+            ry = (px - rx) * nTan + py;
+            xo = -64;
+            yo = -xo * nTan;
+        }
+        if (ra < P2 || ra > P3)
+        {
+            rx = (((int)px >> 6) << 6) + 64;
+            ry = (px - rx) * nTan + py;
+            xo = 64;
+            yo = -xo * nTan;
+        }
+        if (fabs(ra - PI / 2) < 1e-6f || fabs(ra - 3 * PI / 2) < 1e-6f)
+        {
+            rx = px;
+            ry = py;
+            dof = 8;
+        }
         while (dof < 8)
         {
-            mx = (int)(rx) >> 6; my = (int)(ry) >> 6; mp = my * mapX + mx;
-            if (mx >= 0 && mx < mapX && my >= 0 && my < mapY && mp > 0 && map[my][mx] == 1){vx = rx; vy = ry; disV = dist(px, py, vx, vy, ra); dof = 8;}
-            else {rx += xo; ry += yo; dof += 1;}
+            mx = (int)(rx) >> 6;
+            my = (int)(ry) >> 6;
+            if (mx >= 0 && mx < mapX && my >= 0 && my < mapY && map[my][mx] == 1)
+            {
+                vx = rx;
+                vy = ry;
+                disV = dist(px, py, vx, vy, ra);
+                dof = 8;
+            }
+            else
+            {
+                rx += xo;
+                ry += yo;
+                dof += 1;
+            }
         }
-        if (disV<disH) { rx=vx; ry=vy;}
-        if (disH<disV) { rx=hx; ry=hy;}
-        glColor3f(1, 0, 0);
-        glLineWidth(3);
+
+        if (disV < disH)
+        {
+            rx = vx;
+            ry = vy;
+            disT = disV;
+            glColor3f(0.9, 0, 0);  // Vertical wall color
+        }
+        if (disH < disV)
+        {
+            rx = hx;
+            ry = hy;
+            disT = disH;
+            glColor3f(0.7, 0, 0);  // Horizontal wall color
+        }
+
+        glLineWidth(1);
         glBegin(GL_LINES);
         glVertex2i(px, py);
         glVertex2i(rx, ry);
         glEnd();
-        ra += DR; if(ra<0){ra+=2*PI;} if (ra>2*PI) {ra-=2*PI;}
+        
+        // 3D wall 
+        float ca = pa - ra;
+        if (ca < 0) ca += 2 * PI;
+        if (ca > 2 * PI) ca -= 2 * PI;
+        disT = disT * cos(ca);
+        float lineH = (mapS * 320) / disT;
+        if (lineH > 320)
+            lineH = 320;
+        float lineO = 160 - lineH / 2;
+        
+        if (disV < disH)
+            glColor3f(0.9, 0, 0);  // Brighter for vertical walls
+        else
+            glColor3f(0.7, 0, 0);  // Darker for horizontal walls
+            
+        glLineWidth(8);
+        glBegin(GL_LINES);
+        glVertex2i(r * 8 + 530, lineO);
+        glVertex2i(r * 8 + 530, lineO + lineH);
+        glEnd();
+
+        ra += DR;
+        if (ra < 0)
+            ra += 2 * PI;
+        if (ra > 2 * PI)
+            ra -= 2 * PI;
     }
 }
 
